6700be8caf9aa984594c36b5d2988daa724b3683
Anthony2365 <anthonyvinicius124@gmail.com>
2021-05-06 16:01:59 -0300

    laço de repitição WHILE

---
+++ src/br/com/Anthony/LacoDeRepiticaoWhile.java
@@ -0,0 +1,25 @@
+package br.com.Anthony;
+
+public class LacoDeRepiticaoWhile {
+
+	public static void main(String[] args) {
+		int valor = 0;
+		
+		System.out.println("valor: " + valor );
+		
+		while(valor <= 25) {
+			System.out.println("-> valor: " + valor);
+			valor++;
+			//valor +=1;
+			//valor = valor + 1;
+			
+			
+			
+		}
+		
+		System.out.println("Agora o valor é: " + valor );
+
+	}
+
+}
+
